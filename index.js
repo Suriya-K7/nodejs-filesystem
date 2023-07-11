e47182b486d0c7f7540133ca498ea3e8ff62def0
Suriya-K7 <usraising@gmail.com>
2023-07-11 23:21:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// loading required modules
+const { format } = require("date-fns");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+const express = require("express");
+const app = express();
+
+// creating time stamp for file
+const currentDateStamp = `Date-${format(
+  new Date(),
+  "dd-MMM-yyyy"
+)}\t Time-${format(new Date(), "HH:mm:ss")}`;
+
+//creating variable file to store text file
+let textFileInFolder = [];
+
+// creating function for loading folder & files
+const logEvents = async (currentDateStamp) => {
+  const dateTime = `${format(new Date(), "dd-MMM-yyyy")}_time_${format(
+    new Date(),
+    "HH-mm-ss"
+  )}`;
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fsPromises.writeFile(
+      path.join(__dirname, "logs", `${dateTime}.txt`),
+      currentDateStamp
+    );
+    fs.readdir(path.join(__dirname, "logs"), (err, files) => {
+      if (err) {
+        console.error(err);
+      }
+      files.forEach((file) => {
+        if (path.extname(file, path.basename(file)) == ".txt") {
+          textFileInFolder.push(file);
+        }
+      });
+      console.log(textFileInFolder);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+//calling function
+logEvents(currentDateStamp);
+
+app.get("/", function (req, res) {
+  res.send(textFileInFolder);
+});
+
+app.listen(3001);
